9a550aa6aab5a1b67cc372cb136aa57f718af6c2
Francesco Maestrelli <framae26@gmail.com>
2022-01-20 15:18:01 +0100

    attempts

---
+++ main.js
@@ -0,0 +1,21 @@
+
+
+//quanti km devi percorrere?
+
+let distance = parseInt(prompt("Quanti Km devi percorrere?"));
+document.getElementById("distance").innerHTML = distance
+
+
+//alert("ehi");
+
+
+//Quanti hanni hai?
+
+let age = parseInt(prompt("Inserisci la tua età"))
+document.getElementById("age").innerHTML = age
+
+//let priceOfTheTicket = (1 = 0,21)
+//let age = parseInt(prompt("Inserisci l'età"))
+//if (age < 18) {
+ //   result = ((1 = 0,21) - 20%)
+//}
