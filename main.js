573b51f7f0acab99438d897e4346ab784d972380
Francesco Maestrelli <framae26@gmail.com>
2022-01-20 15:26:53 +0100

    price

--- main.js
+++ main.js
@@ -6,13 +6,20 @@ let distance = parseInt(prompt("Quanti Km devi percorrere?"));
 document.getElementById("distance").innerHTML = distance
 
 
-//alert("ehi");
+//prezzo del biglietto al km
+
+let pricePerKm = parseInt(0.21);
 
 
 //Quanti hanni hai?
 
 let age = parseInt(prompt("Inserisci la tua età"))
 document.getElementById("age").innerHTML = age
+
+
+let result = 0.21 * distance * age
+document.getElementById("result").innerHTML = result
+
 
 //let priceOfTheTicket = (1 = 0,21)
 //let age = parseInt(prompt("Inserisci l'età"))
